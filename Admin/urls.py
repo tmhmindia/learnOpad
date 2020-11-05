32dd147edb3cf7302fb341afab04b19fcc6467d2
Vijay gwala <55628967+vijaygwala@users.noreply.github.com>
2020-11-05 13:27:31 +0530

    staged

--- Admin/urls.py
+++ Admin/urls.py
@@ -5,6 +5,16 @@ urlpatterns = [
     path('learnopad/admin/manage/learners', views.manage_learners, name='manage_learners'),
     path('learnopad/admin/edit/learners/', views.view_edit_learners, name='view_edit_learners'),
     path('learnopad/admin/dashboard/', views.Dashboard, name='dashboard_admin'),
+    path('learnopad/admin/manage/Courses/', views.manage_courses, name='manage_courses'),
+    path('learnopad/admin/manage/facilitators/', views.manage_facilitators, name='manage_facilitators'),
+    path('learnopad/admin/edit/facilitators/', views.view_edit_facilitators, name='view_edit_facilitators'),
+    path('learnopad/admin/edit/Courses/', views.view_edit_courses, name='view_edit_courses'),
+    path('learnopad/admin/Approved/Courses/', views.Approved_courses, name='Approved_courses'),
+    path('learnopad/admin/Approved/facilitators/', views.Approved_facilitators, name='Approved_facilitators'),
+
+
+
+
 
 
 ]
--- Admin/views.py
+++ Admin/views.py
@@ -3,9 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 def Dashboard(request):
     return render(request,'myAdmin/dashboard/index.html')
+def manage_facilitators(request):
+    return render(request,'myAdmin/dashboard/manage_facilitators.html')
+def Approved_facilitators(request):
+    return render(request,'myAdmin/dashboard/aprooved_facilitators.html')
+def Approved_courses(request):
+    return render(request,'myAdmin/dashboard/aprooved_courses.html')
 
+def view_edit_facilitators(request):
+    return render(request,'myAdmin/dashboard/view_edit_facilitators.html')
+def view_edit_courses(request):
+    return render(request,'myAdmin/dashboard/view_edit_courses.html')
 def manage_learners(request):
     return render(request,'myAdmin/dashboard/manage_learners.html')
+def manage_courses(request):
+    return render(request,'myAdmin/dashboard/manage_course.html')
+
 
 def view_edit_learners(request):
     return render(request,'myAdmin/dashboard/view_edit_learners.html')
