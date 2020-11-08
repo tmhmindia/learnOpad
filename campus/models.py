13741146dbe5a24bb173ac3398f3db2b5d3de41e
Vijay gwala <55628967+vijaygwala@users.noreply.github.com>
2020-11-08 18:03:05 +0530

    merged

--- campus/models.py
+++ campus/models.py
@@ -3,17 +3,10 @@ from LandingPage.models import Course
 
 # Create your models here.
 class Campus(models.Model):
-<<<<<<< HEAD
     name=models.CharField(max_length=100)
     email=models.EmailField(max_length=100)
     campus=models.CharField(max_length=250)
     course= models.ForeignKey(Course,related_name='campus_course', on_delete=models.CASCADE,null=True)
-=======
-    name=models.CharField(max_length=50)
-    email=models.EmailField(max_length=100)
-    mobile=models.CharField(max_length=10)
-    course_name=models.CharField(max_length=200)
->>>>>>> 15d95099dc2131a4d7e1f6981b2e7cc020ca264c
    
 
     def __str__(self):
