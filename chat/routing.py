946ebef488e6048179590f2e48686fb40cc54f90
Vijay gwala <55628967+vijaygwala@users.noreply.github.com>
2020-12-16 15:38:02 +0530

    staged

--- chat/routing.py
+++ chat/routing.py
@@ -5,5 +5,5 @@ from .consumers import ChatConsumer
 
 
 websocket_urlpatterns = [
-    re_path(r'^ws/chat/(?P<room_name>[^/]+)/$', ChatConsumer.as_asgi()),
+    re_path(r'^ws/chat/(?P<room_name>[^/]+)/$', ChatConsumer),
 ]
